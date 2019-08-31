31216b7dd6365191ac5e89129adc385bd1ea8b61
CasonWebDev <joao@cason.com.br>
2019-08-31 20:33:26 -0300

    Continuação do Desenvolvimento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '3d745420bd4191',
+    pass: 'acc15f547dc753',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
