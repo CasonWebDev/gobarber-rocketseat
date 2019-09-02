9773a98b9fca973b0fc1a98751275c4a50d93cdb
CasonWebDev <joao@cason.com.br>
2019-09-01 23:57:29 -0300

    Finalizando API

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schema/Notification';
 
-import Mail from '../../lib/Mail';
+import CancelationMail from '../jobs/CancelationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -15,7 +16,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
@@ -141,6 +142,12 @@ class AppointmentController {
       });
     }
 
+    if (appointment.canceled_at) {
+      return res.status(401).json({
+        error: 'You already make the cancelation of this appointment',
+      });
+    }
+
     const dateWithSub = subHours(appointment.date, 2);
 
     if (isBefore(dateWithSub, new Date())) {
@@ -153,17 +160,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancelation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: ptBR,
-        }),
-      },
+    await Queue.add(CancelationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
